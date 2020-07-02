c54b666d37b8c8e14f5791a47a9e08e1fa16af81
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-07-02 14:02:40 +0530

    Frist time pushing eclise to github

---
+++ smapleSpringBoot/src/main/java/com/smaple/smapleSpringBoot/SmapleSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.smaple.smapleSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapleSpringBootApplication.class, args);
+	}
+
+}
---
+++ smapleSpringBoot/src/test/java/com/smaple/smapleSpringBoot/SmapleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smaple.smapleSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmapleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
